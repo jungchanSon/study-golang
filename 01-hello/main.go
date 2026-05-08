c6fc197a54b2239e149d55451172cc3ee3dc41de
sonjungchan <rnrmfjc@gmail.com>
2026-05-08 19:52:53 +0900

    feat/01-hello: Add self introduction
    Signed-off-by: sonjungchan <rnrmfjc@gmail.com>

--- 01-hello/main.go
+++ 01-hello/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go!")
-	fmt.Println("Go 프로그램은 package main의 main 함수에서 시작합니다.")
+	fmt.Println("안녕하세요~")
+	fmt.Println("손정찬입니다.")
+	fmt.Println("잘 부탁드립니다! 화이팅!")
+
 }
